e8db8bb038970fd395278f5ac43a6b61c68fe057
mani-87-12 <gopalreddytheluckier@gmail.com>
2024-11-10 16:28:06 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,17 @@
+//firebase console api keys
+
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzl7vzqCzwEULVnpuOE3N-k7DYOvVDGRs",
-  authDomain: "socialmedia-5d2e5.firebaseapp.com",
-  projectId: "socialmedia-5d2e5",
-  storageBucket: "socialmedia-5d2e5.appspot.com",
-  messagingSenderId: "420100686230",
-  appId: "1:420100686230:web:0e35a414b9b756a7099db7",
-  measurementId: "G-KK8HNGMX60"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
